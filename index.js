45426c3886e73956c4b25c6763d7df549b24f869
Márton Borlay <marton.borlay@kriek.hu>
2015-02-18 12:26:34 +0100

    New module api

--- index.js
+++ index.js
@@ -1,115 +1,112 @@
 var fs = require("fs");
 _ = require("lodash");
 
-/**
- * The actual data is stored here
- * @type {Object}
- * @private
- */
-var data = {};
+module.exports =  function(options) {
+	options = _.extend({
+		file: "data/data.json"
+	}, options);
 
-var options;
+	return function(callback) {
 
-function syncData() {
-	fs.writeFile(options.namespace, JSON.stringify(data, null, "\t"), function(err) {
-		if (err) {
-			console.log(err);
-		}
-	});
-}
-
-
-module.exports = {
-	initialize: function(callback) {
 		//Set up memory by reading the contents of the file
-		if (!fs.existsSync(this.options.namespace)) {
-			fs.writeFileSync(this.options.namespace, "{\"_contents\": {}}");
+		if (!fs.existsSync(options.file)) {
+			fs.writeFileSync(options.file, "{}");
 		}
 
-		data = JSON.parse(fs.readFileSync(this.options.namespace, {
+		var _data = JSON.parse(fs.readFileSync(options.file, {
 			encoding: 'utf-8'
 		}));
 
-		options = this.options;
-
-		callback && callback();
-	},
-	dataStore: {
-		get: function(path, callback) {
-			path = _.filter(path.split("/"), function(string) {
-				return string !== "";
-			});
-			var node = data;
-			//Loop through the data by the given paths
-			for (var i = 0; i < path.length; i++) {
-				var temp = node[path[i]];
-				//If we found the given key, assign the node object to its value
-				if (temp !== undefined) {
-					node = node[path[i]];
-					//If the given key doesn't exist, try the _id
-				} else {
-					temp = _.find(node, function(obj) {
-						return obj._id === path[i];
-					});
-					//If it's not undefined, then assign it as the value
-					if (temp !== undefined) {
-						node = temp;
-					} else {
-						callback({
-							status: 404,
-							error: "Requested node doesn't exist."
-						}, null);
-						return;
-					}
+		/**
+		 * Sync the data currently in memory to the target file
+		 */
+		function syncData() {
+			fs.writeFile(options.file, JSON.stringify(_data, null, "\t"), function(err) {
+				if (err) {
+					console.log(err);
 				}
-			}
-
-			callback(null, node);
-		},
-		set: function(path, data, callback) {
-			var self = this;
-			this.get(path, function(err, target) {
-				if (_.isArray(target)) {
-					target.push(data);
-					callback();
-					syncData();
-				} else if (_.isObject(target)) {
-					target = _.extend(target, data);
-					callback();
-					syncData();
-				} else {
-					self.parent(path, function(err, parent) {
-						parent[_.last(path.split("/"))] = data;
-						callback();
-						syncData();
-					});
-				}
-			});
-		},
-		delete: function(path, callback) {
-			var index = _.last(path.split("/"));
-			this.parent(path, function(err, parent) {
-				if (_.isArray(parent)) {
-					if (_.isNumber(Number(index)) && !_.isNaN(Number(index))) {
-						parent.splice(index, 1);
-						//If it's a hash (string), we find the target object, get it's index and remove it from the array that way
-					} else {
-						var obj = _.find(parent, function(obj) {
-							return obj._id === index;
-						});
-						parent.splice(_.indexOf(parent, obj), 1);
-					}
-				} else if (_.isObject(parent)) {
-					delete parent[index];
-				}
-				callback();
-				syncData();
-			});
-		},
-		parent: function(path, callback) {
-			this.get(path.split("/").slice(0, -1).join("/"), function(err, parent) {
-				callback(err, parent);
 			});
 		}
+
+		/**
+		 * Return the datastore 
+		 */
+		return callback && typeof callback === 'function' && callback(null, {
+			get: function(path, callback) {
+				path = _.filter(path.split("/"), function(string) {
+					return string !== "";
+				});
+				var node = _data;
+				//Loop through the data by the given paths
+				for (var i = 0; i < path.length; i++) {
+					var temp = node[path[i]];
+					//If we found the given key, assign the node object to its value
+					if (temp !== undefined) {
+						node = node[path[i]];
+						//If the given key doesn't exist, try the _id
+					} else {
+						temp = _.find(node, function(obj) {
+							return obj._id === path[i];
+						});
+						//If it's not undefined, then assign it as the value
+						if (temp !== undefined) {
+							node = temp;
+						} else {
+							return callback && typeof callback === 'function' && callback({
+								status: 404,
+								error: "Requested node doesn't exist."
+							}, null);
+						}
+					}
+				}
+
+				return callback && typeof callback === 'function' && callback(null, node);
+			},
+			set: function(path, data, callback) {
+				var self = this;
+				this.get(path, function(err, target) {
+					if (_.isArray(target)) {
+						target.push(data);
+						callback && typeof callback === 'function' && callback(null);
+						syncData();
+					} else if (_.isObject(target)) {
+						target = _.extend(target, data);
+						callback && typeof callback === 'function' && callback(null);
+						syncData();
+					} else {
+						self.parent(path, function(err, parent) {
+							parent[_.last(path.split("/"))] = data;
+							callback && typeof callback === 'function' && callback(null);
+							syncData();
+						});
+					}
+				});
+			},
+			delete: function(path, callback) {
+				var index = _.last(path.split("/"));
+				this.parent(path, function(err, parent) {
+					if (_.isArray(parent)) {
+						if (_.isNumber(Number(index)) && !_.isNaN(Number(index))) {
+							parent.splice(index, 1);
+							//If it's a hash (string), we find the target object, get it's index and remove it from the array that way
+						} else {
+							var obj = _.find(parent, function(obj) {
+								return obj._id === index;
+							});
+							parent.splice(_.indexOf(parent, obj), 1);
+						}
+					} else if (_.isObject(parent)) {
+						delete parent[index];
+					}
+					callback && typeof callback === 'function' && callback(null);
+					syncData();
+				});
+			},
+			parent: function(path, callback) {
+				this.get(path.split("/").slice(0, -1).join("/"), function(err, parent) {
+					callback && typeof callback === 'function' && callback(err, parent);
+				});
+			}
+		});
 	}
 }
--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-var Cantrip = require("Cantrip");
-var json = require("./index.js");
-Cantrip.options.persistence = json;
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser')
+var jsonPersistence = require("./index");
 
-Cantrip.start();
+require("Cantrip")({
+	persistence: jsonPersistence()
+}, function(err, cantrip) {
+
+	var app = express();
+	app.use(bodyParser.json());
+	app.use(function(err, req, res, next) {
+		return next({
+			status: 400,
+			error: "Invalid JSON supplied in request body."
+		});
+	});
+
+	app.use(bodyParser.urlencoded());
+	app.use(cors());
+
+	app.use(cantrip);
+
+	app.use(function(req, res, next) {
+		res.send(res.body);
+	});
+
+	app.use(function(err, req, res, next) {
+		if (err.status) res.status(err.status);
+		res.send({
+			error: err.error
+		});
+	});
+
+	app.listen(3000);
+
+});
+
+
